82b07da26742599894e4291839780de729dcf0ef
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-08 11:21:01 +0300

    Adding saveNewAuthor implementation and test. Refactoring AuthorMapper.

--- src/main/java/merkanto/sdjpajdbctemplate/dao/AuthorDaoImpl.java
+++ src/main/java/merkanto/sdjpajdbctemplate/dao/AuthorDaoImpl.java
@@ -22,13 +22,18 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
         return jdbcTemplate.queryForObject("SELECT * FROM author WHERE first_name = ? and last_name = ?",
-                                                getRowMapper(),
-                                                firstName, lastName);
+                getRowMapper(),
+                firstName, lastName);
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        jdbcTemplate.update("INSERT INTO author (first_name, last_name) VALUES (?, ?)",
+                author.getFirstName(), author.getLastName());
+
+        Long createdId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
+
+        return this.getById(createdId);
     }
 
     @Override
@@ -41,7 +46,8 @@ public class AuthorDaoImpl implements AuthorDao {
 
     }
 
-    private RowMapper<Author> getRowMapper() {
+    private RowMapper<Author> getRowMapper(){
         return new AuthorMapper();
     }
+
 }
